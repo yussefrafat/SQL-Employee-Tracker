69da0b765356e7bb24012a7dec159168effd2aaf
Yussef Rafat <yussefrafat5908@gmail.com>
2021-08-14 15:13:50 -0400

    tables

--- schema.sql
+++ schema.sql
@@ -14,6 +14,18 @@ CREATE TABLE departement (
 -- Create the table for roles
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    salary DECIMAL NOT NULL,
+    departement_id INT NOT NULL,
+    PRIMARY KEY (id)
+);
+
+-- Create the table for employees
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
     PRIMARY KEY (id)
 );
